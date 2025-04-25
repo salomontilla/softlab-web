7f2642513e5d777088e0ccfcfef56f2171b3e33a
salo <salomontilla@outlook.com>
2025-04-25 16:24:03 -0500

    crea el github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://salomontilla.github.io/softlab-web',
+  base: '/softlab-web/',
 });
