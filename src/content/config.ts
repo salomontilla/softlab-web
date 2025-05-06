b54e9ae35729a9f66a2482b530f54a5663a2ece4
salo <salomontilla@outlook.com>
2025-05-06 14:42:38 -0500

    cambia la configuracion de los schemas

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,21 @@
-import { defineCollection, z } from "astro:content"; //Zod es una libreria para validar schemas en este caso los proyectos
+import { defineCollection, z } from "astro:content";
+
+const autorSchema = z.object({
+  img_autor: z.string(),
+  autor: z.string(),
+  programa: z.string(),
+  modalidad: z.string(),
+  social: z.record(z.string(), z.string()).optional(), 
+});
 
 const proyectos = defineCollection({
-    schema: z.object({
-        img_autor: z.string(),
-        autor: z.string(),
-        programa: z.string(),
-        modalidad: z.string(),
-        social: z.record(z.string(), z.string()),
-        img_producto: z.string(),
-        nombre_producto: z.string(),
-        descripcion: z.string(),
-        tecnologias: z.record(z.string(), z.string()),
-    })
-})
-export const collections = {proyectos}
+  schema: z.object({
+    autores: z.record(autorSchema),
+    img_producto: z.string(),
+    nombre_producto: z.string(),
+    descripcion: z.string(),
+    tecnologias: z.record(z.string(), z.string()),
+  }),
+});
+
+export const collections = { proyectos };
